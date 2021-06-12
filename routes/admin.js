999f1b79c1bd211c49a55373df2c639c01ebd0f7
hadipsy27 <hadipsy27@gmail.com>
2021-06-12 07:50:11 +0700

    create cateogry (CRUD)

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,11 @@ const adminController = require('../controllers/adminController')
 
 router.get('/dashboard', adminController.viewDashboard)
 router.get('/category', adminController.viewCategory)
+router.post('/category', adminController.addCategory)
+
 router.get('/bank', adminController.viewBank)
 router.get('/item', adminController.viewItem)
 router.get('/booking', adminController.viewBooking)
 
 module.exports = router
+
