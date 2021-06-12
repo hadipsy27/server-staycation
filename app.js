30a9ab815e90e8d8d359036fbcb66f0d0b061e68
hadipsy27 <hadipsy27@gmail.com>
2021-06-12 17:04:28 +0700

    4.6.2-04 update category part 2 (CRUD)

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override'); // npm install method-override
+
 // import mongoose
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/db_mern', {
@@ -24,6 +26,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -18,6 +18,16 @@ module.exports = {
     res.redirect('/admin/category')
   },
 
+  editCategory: async (req, res) => {
+    const {id, name} = req.body;
+    // console.log(id)
+    const category = await Category.findOne({_id: id})
+    // console.log(category);
+    category.name = name;
+    await category.save();
+    res.redirect('/admin/category')
+  },
+
   viewBank: function (req, res) {
     res.render('admin/bank/view_bank')
   },
