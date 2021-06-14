8f0c52451fffed4822b23b11ff4f6e9d1237d1bd
hadipsy27 <hadipsy27@gmail.com>
2021-06-14 21:28:12 +0700

    4.6.4-02 Read Bank (CRUD)

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -75,15 +75,17 @@ module.exports = {
     }
   },
 
-  viewBank: function (req, res) {
+  viewBank: async function (req, res) {
     try {
+      const bank = await Bank.find()
       const alertMessage = req.flash('alertMessage')
       const alertStatus = req.flash('alertStatus')
       const alert = {message: alertMessage, status: alertStatus}
       res.render('admin/bank/view_bank', 
       {
         title: 'Staycation | Bank',
-        alert
+        alert,
+        bank
       })
     } catch (error) {
       req.flash('alertMessage',`${error.message}`)
