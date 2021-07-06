3c6b63ab44edb28da5b03805aa723419c588ac92
hadipsy27 <hadipsy27@gmail.com>
2021-07-06 14:17:39 +0700

    4.6.6 05 Delete Feature with images

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -424,6 +424,29 @@ module.exports = {
     }
   },
 
+  deleteFeature: async (req, res) => {
+    const { id,itemId } = req.params
+    try {
+      const feature = await Feature.findOne({ _id: id })
+      const item = await Item.findOne({ _id: itemId}).populate('featureId')
+      for(let i = 0; i < feature.length; i++){
+        if(item.feature[i]._id.toString() === feature._id.toString()){
+          item.feature.pull({_id : feature._id})
+          await item.save()
+        }
+      }
+      await fs.unlink(path.join(`public/${feature.imageUrl}`))
+      await feature.remove()
+      req.flash('alertMessage','Success Delete Feature')
+      req.flash('alertStatus', 'success')
+      res.redirect(`/admin/item/show-detail-item/${itemId}`)
+    } catch (error) {
+      req.flash('alertMessage',`${error.message}`)
+      req.flash('alertStatus', `danger`)
+      res.redirect('/admin/Feauter') 
+    }
+  },
+
   viewBooking: (req, res) => {
     res.render('admin/booking/view_booking', {title: 'Staycation | Booking'})
   }
