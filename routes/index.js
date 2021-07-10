34c84e3f553d16a668d3da02786d0491495016b8
hadipsy27 <hadipsy27@gmail.com>
2021-07-10 14:58:06 +0700

    4.6.8 02 - login admin dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin/dashboard/view_dashboard', { title: 'Dashboard' });
+  // res.render('index', { title: 'express' });
+  res.redirect('admin/signin')
 });
 
 module.exports = router;
