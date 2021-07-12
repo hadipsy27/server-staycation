593e3161a64fe812fe020aca2dbe1da576f293b8
hadipsy27 <hadipsy27@gmail.com>
2021-07-12 21:08:38 +0700

    4.6.9 03 - action confirmation & reject booking

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -618,6 +618,9 @@ module.exports = {
   showDetailBooking: async (req, res) => {
     const { id } = req.params
     try {
+      const alertMessage = req.flash('alertMessage')
+      const alertStatus = req.flash('alertStatus')
+      const alert = {message: alertMessage, status: alertStatus}
       const booking = await Booking.findOne({ _id: id })
         .populate('memberId')
         .populate('bankId')
@@ -625,10 +628,40 @@ module.exports = {
       res.render('admin/booking/show_detail_booking', {
         title: "Staycation | Detail Booking",
         user: req.session.user,
-        booking
+        booking,
+        alert
       })
     } catch (error) {
       res.redirect('/admin/booking')
+    }
+  },
+
+  actionConfirmation: async (req, res) => {
+    const { id } = req.params
+    try {
+      const booking = await Booking.findOne({ _id: id })
+      booking.payments.status == 'Accept'
+      await booking.save()
+      req.flash('alertMessage','Success Confirmation Booking')
+      req.flash('alertStatus', 'success')
+      res.redirect(`/admin/booking/${id}`)
+    } catch (error) {
+      res.redirect(`/admin/booking/${id}`)
+    }
+  },
+
+  actionReject: async (req, res) => {
+    const { id } = req.params
+    try {
+      const booking = await Booking.findOne({ _id: id })
+      booking.payments.status = 'Reject'
+      await booking.save()
+      // console.log(booking.payments.status)
+      req.flash('alertMessage','Success Reject Booking')
+      req.flash('alertStatus', 'success')
+      res.redirect(`/admin/booking/${id}`)
+    } catch (error) {
+      res.redirect(`/admin/booking/${id}`)
     }
   }
 }
